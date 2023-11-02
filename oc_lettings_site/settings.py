628d9e45fff7ba034d944042e644b035c7e91a75
Benjamin B <dev@benjamin-b.fr>
2023-11-02 17:06:05 +0100

    Create lettings app with own models

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'oc_lettings_site.apps.OCLettingsSiteConfig',
+    'lettings.apps.LettingsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
