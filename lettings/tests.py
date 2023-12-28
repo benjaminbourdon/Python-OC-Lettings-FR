1fbc72881f0a82e229af5e9cc392bf72e71ddbd2
Benjamin B <dev@benjamin-b.fr>
2023-12-28 18:09:21 +0100

    Complete tests on lettings and profiles modules

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,87 @@
-from django.test import TestCase
+import pytest
+from django.test import Client
+from django.urls import resolve, reverse
+from pytest_django.asserts import assertTemplateUsed
 
-# Create your tests here.
+from .models import Address, Letting
+
+
+@pytest.mark.django_db
+def test_lettings_models():
+    adress = Address.objects.create(
+        number=2, street="main street", city="megalopole", zip_code=10000
+    )
+    expected_adress = "2 main street"
+    assert str(adress) == expected_adress
+
+    letting = Letting.objects.create(title="Big House", address=adress)
+    expected_letting = "Big House"
+    assert str(letting) == expected_letting
+
+
+def test_index_lettings_url():
+    path = reverse("lettings_index")
+
+    assert path == "/lettings/"
+    assert resolve(path).view_name == "lettings_index"
+
+
+@pytest.mark.django_db
+def test_index_lettings_view_no_letting(client: Client):
+    path = reverse("lettings_index")
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert "Lettings</h1>" in response.content.decode()
+    assert "<p>No lettings are available.</p>" in response.content.decode()
+    assertTemplateUsed(response, "lettings/index.html")
+
+
+@pytest.mark.django_db
+def test_index_lettings_view_with_letting(client: Client):
+    letting_title = "Big House"
+    adress = Address.objects.create(
+        number=2, street="main street", city="megalopole", zip_code=10000
+    )
+    letting = Letting.objects.create(title=letting_title, address=adress)
+
+    path = reverse("lettings_index")
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert "Lettings</h1>" in response.content.decode()
+    assert (
+        f'<a href="/lettings/{letting.id}/">{letting_title}</a>'
+        in response.content.decode()
+    )
+    assertTemplateUsed(response, "lettings/index.html")
+
+
+@pytest.mark.django_db
+def test_display_lettings_url():
+    letting_title = "Big House"
+    adress = Address.objects.create(
+        number=2, street="main street", city="megalopole", zip_code=10000
+    )
+    letting = Letting.objects.create(title=letting_title, address=adress)
+    path = reverse("letting", args={letting.id})
+
+    assert path == f"/lettings/{letting.id}/"
+    assert resolve(path).view_name == "letting"
+
+
+@pytest.mark.django_db
+def test_display_lettings_view(client: Client):
+    letting_title = "Big House"
+    adress = Address.objects.create(
+        number=2, street="main street", city="megalopole", zip_code=10000
+    )
+    letting = Letting.objects.create(title=letting_title, address=adress)
+    path = reverse("letting", args={letting.id})
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert f"<title>{letting_title}</title>" in response.content.decode()
+    assert f"<p>{adress.number} {adress.street}</p>" in response.content.decode()
+    assert f"{letting_title}</h1>" in response.content.decode()
+    assertTemplateUsed(response, "lettings/letting.html")
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,81 @@
-from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from django.test import Client
+from django.urls import resolve, reverse
+from pytest_django.asserts import assertTemplateUsed
 
-# Create your tests here.
+from .models import Profile
+
+
+@pytest.mark.django_db
+def test_profile_model():
+    user = User.objects.create(username="Coco")
+    profile = Profile.objects.create(user=user, favorite_city="Paris")
+    expected_adress = "Coco"
+    assert str(profile) == expected_adress
+
+
+def test_index_profiles_url():
+    path = reverse("profiles_index")
+
+    assert path == "/profiles/"
+    assert resolve(path).view_name == "profiles_index"
+
+
+@pytest.mark.django_db
+def test_index_profiles_view_no_profile(client: Client):
+    path = reverse("profiles_index")
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert "Profiles</h1>" in response.content.decode()
+    assert "<p>No profiles are available.</p>" in response.content.decode()
+    assertTemplateUsed(response, "profiles/index.html")
+
+
+@pytest.mark.django_db
+def test_index_profiles_view_with_profile(client: Client):
+    username = "Coco"
+    user = User.objects.create(username=username)
+    Profile.objects.create(user=user, favorite_city="Paris")
+
+    path = reverse("profiles_index")
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert "Profiles</h1>" in response.content.decode()
+    assert (
+        f'<a href="/profiles/{username}/">{username.capitalize()}</a>'
+        in response.content.decode()
+    )
+    assertTemplateUsed(response, "profiles/index.html")
+
+
+@pytest.mark.django_db
+def test_display_profiles_url():
+    username = "Coco"
+    user = User.objects.create(username=username)
+    Profile.objects.create(user=user, favorite_city="Paris")
+    path = reverse("profile", args={username})
+
+    assert path == f"/profiles/{username}/"
+    assert resolve(path).view_name == "profile"
+
+
+@pytest.mark.django_db
+def test_display_profiles_view(client: Client):
+    username = "Coco"
+    city = "Paris"
+    user = User.objects.create(username=username)
+    Profile.objects.create(user=user, favorite_city=city)
+
+    path = reverse("profile", args={username})
+    response = client.get(path)
+
+    assert response.status_code == 200
+    assert f"<title>{username}</title>" in response.content.decode()
+    assert (
+        f"<p><strong>Favorite city :</strong> {city}</p>" in response.content.decode()
+    )
+    assert f"{username}</h1>" in response.content.decode()
+    assertTemplateUsed(response, "profiles/profile.html")
